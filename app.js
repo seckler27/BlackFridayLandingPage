ddb73d5a9de4f486bc8d7c31adb745719aed9f49
davi <davi.seckler@gmail.com>
2021-07-15 17:14:03 -0300

    acerto das imagens

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const emailForm = document.getElementById('email-form');
 emailForm.addEventListener('submit', (event) => {
-    let name = document.getElementById('name-box').value;
-    let email = document.getElementById('email-box').value;      
+    const name = document.getElementById('name-box').value;
+    const email = document.getElementById('email-box').value;      
         const data = {
             email,
             name
         } 
-        let dataArray = JSON.parse(localStorage.getItem('lead')) || [];
+        const dataArray = JSON.parse(localStorage.getItem('lead')) || [];
         dataArray.push(data);
-        let convertedData = JSON.stringify(dataArray);
+        const convertedData = JSON.stringify(dataArray);
         localStorage.setItem('lead', convertedData);
-        alert('Cadastro efetuado com sucesso!');
+        alert('Cadastro efetuado com sucesso! Muito obrigado!');
     }
 );
 
