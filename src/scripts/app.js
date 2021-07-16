2abd9918cd5d1cfaec730d14798702dda703320b
davi <davi.seckler@gmail.com>
2021-07-16 16:47:05 -0300

    teste

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,11 +6,12 @@ emailForm.addEventListener('submit', (event) => {
             email,
             name
         } 
-        const dataArray = (JSON.parse(localStorage.getItem('lead')) ? JSON.parse(localStorage.getItem('lead')) : []);
-        dataArray.push(data);
-        const convertedData = JSON.stringify(dataArray);
+        const convertedData = JSON.stringify(data);
+        // const dataArray = (JSON.parse(localStorage.getItem('lead')) ? JSON.parse(localStorage.getItem('lead')) : []);
+        // dataArray.push(data);
+        // const convertedData = JSON.stringify(dataArray);
         localStorage.setItem('lead', convertedData);
-        const audio = document.getElementById('audio');
+        const audio = document.getElementById("audio");
         audio.play();
        alert('Cadastro efetuado com sucesso! Muito obrigado!');
         
