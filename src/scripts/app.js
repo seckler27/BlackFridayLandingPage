e9ec7a9f9c21cd0d6b145f7d8eb656058dd883d3
davi <davi.seckler@gmail.com>
2021-07-16 18:23:58 -0300

    final test

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,31 +1,13 @@
 const emailForm = document.getElementById('email-form');
 emailForm.addEventListener('submit', (event) => {
     const name = document.getElementById('name-box').value;
-    const email = document.getElementById('email-box').value;      
-        const data = {
-            email,
-            name
-        } 
-        const convertedData = JSON.stringify(data);
-        // const dataArray = (JSON.parse(localStorage.getItem('lead')) ? JSON.parse(localStorage.getItem('lead')) : []);
-        // dataArray.push(data);
-        // const convertedData = JSON.stringify(dataArray);
-        localStorage.setItem('lead', convertedData);
-        const audio = document.getElementById("audio");
-        audio.play();
-       alert('Cadastro efetuado com sucesso! Muito obrigado!');
-        
+    const email = document.getElementById('email-box').value;
+    const data = {
+        email,
+        name
     }
+       const convertedData = JSON.stringify(data);
+    localStorage.setItem('lead', convertedData);
+    alert('Cadastro efetuado com sucesso! Muito obrigado!');
+}
 );
-
-
-
-
-
-
-
-
-
-
-
-
